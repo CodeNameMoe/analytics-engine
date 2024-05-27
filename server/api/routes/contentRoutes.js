14d9f31b0860f600ab37af8ead48645dd91f9c91
codenamemoe <newmoeali@gmail.com>
2024-05-27 10:15:35 +0100

    file name fix

--- server/api/routes/contentRoutes.js
+++ server/api/routes/contentRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import { getByType } from '../routeHandlers/contentHandlers/byType.js';
-import { getByCreator } from '../routeHandlers/contentHandlers/ByCreator.js';
-import { getByPublicationDate } from '../routeHandlers/contentHandlers/ByPublicationDate.js';
-import { getByOrganisation } from '../routeHandlers/contentHandlers/ByOrganisation.js';
+import { getByCreator } from '../routeHandlers/contentHandlers/byCreator.js';
+import { getByPublicationDate } from '../routeHandlers/contentHandlers/byPublicationDate.js';
+import { getByOrganisation } from '../routeHandlers/contentHandlers/byOrganisation.js';
 
 const router = express.Router();
 
