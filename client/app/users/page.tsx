f8b33a2f6f06205da0be35ae741936cf3d02461c
codenamemoe <newmoeali@gmail.com>
2024-05-27 16:05:53 +0100

    under construction card

--- client/app/users/page.tsx
+++ client/app/users/page.tsx
@@ -1,53 +1,18 @@
-"use client"
- 
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-import { useEffect, useState } from "react";
 
-function Page() {
-  const [data, setData] = useState({});
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
-  useEffect(() => {
-    fetch('http://localhost:3001/organizations/by-industry')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => setData(data))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
+export default function Page() {
+  
+
   return (
-    <div>
-      <div className="xxd ">
-      <h1>Organisations</h1>
-      </div>
-
-      <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select a fruit" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Fruits</SelectLabel>
-          <SelectItem value="apple">Apple</SelectItem>
-          <SelectItem value="banana">Banana</SelectItem>
-          <SelectItem value="blueberry">Blueberry</SelectItem>
-          <SelectItem value="grapes">Grapes</SelectItem>
-          <SelectItem value="pineapple">Pineapple</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <div className="p-4">
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle className='text-center'>🚧 UNDER CONSTRUCTION 🚧</CardTitle>
+        </CardHeader>
+        <CardContent className="pl-2">
+        </CardContent>
+      </Card>
     </div>
-  )
+  );
 }
-
-export default Page
